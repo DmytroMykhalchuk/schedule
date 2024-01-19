5b66592299254f5ff893a6feedca1efcd793a7e7
Dima Mykhalchuk <dimamuchlachuk@gmail.com>
2024-01-19 19:45:09 +0200

    Added calendar.

--- src/app/Componnets/Home/HomeCalendar.tsx
+++ src/app/Componnets/Home/HomeCalendar.tsx
@@ -1,13 +1,21 @@
 import Paper from "@mui/material/Paper";
+import 'react-calendar/dist/Calendar.css';
+import Typography from '@mui/material/Typography'
+// import Calendar from 'react-calendar';
+import { LinkedDateCalendar } from "./LinkedDateCalendar";
 
 type HomeCalendarType = {
 };
 
 export const HomeCalendar: React.FC<HomeCalendarType> = ({ }) => {
-
     return (
         <>
-            <Paper elevation={4} sx={{ p: 2 }}>Calendar</Paper>
+            <Paper elevation={4} sx={{ p: 2 }}>
+                <Typography variant="subtitle2">
+                    Calendar
+                </Typography>
+                <LinkedDateCalendar />
+            </Paper>
         </>
     );
 };
